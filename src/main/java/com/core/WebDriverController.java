0d00a92d4f50dfeeea4db27e7bdef768230d2d4c
Dipjyoti Metia <dipjyoti.metia@beteasy.com.au>
2019-01-15 13:19:34 +1100

    Adding performance testing

--- src/main/java/com/core/WebDriverController.java
+++ src/main/java/com/core/WebDriverController.java
@@ -1,41 +1,91 @@
 package com.core;
 
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.CaptureType;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.io.FileOutputStream;
+import java.net.Inet4Address;
 import java.net.URL;
 
 
 public class WebDriverController {
 
     private static WebDriver _driverThread = null;
+    public static BrowserMobProxyServer proxy;
 
-    @Parameters({"browser","grid"})
+    @Parameters({"browser", "grid" ,"perf"})
     @BeforeClass
-    public void setup(String browser,String grid){
-        initDriver(browser,grid);
+    public void setup(String browser, String grid, String perf) {
+        initDriver(browser, grid, perf);
     }
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return _driverThread;
     }
 
+    private synchronized void initDriver(String browser, String grid ,String perf) {
+        try {
+            if (browser.equals("firefox")) {
+                _driverThread = new FirefoxDriver(getFirefoxOptions());
+                if (grid.equalsIgnoreCase("YES")) {
+                    _driverThread = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), getFirefoxOptions());
+                }
+            } else if (browser.equals("chrome")) {
+                _driverThread = new ChromeDriver(getChromeOptions(perf));
+                if (grid.equalsIgnoreCase("YES")) {
+                    _driverThread = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), getChromeOptions(perf));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DesiredCapabilities performance() {
+        proxy = new BrowserMobProxyServer();
+        proxy.start();
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        try {
+            String hostIp = Inet4Address.getLocalHost().getHostAddress();
+            seleniumProxy.setHttpProxy(hostIp + ":" + proxy.getPort());
+            seleniumProxy.setSslProxy(hostIp + ":" + proxy.getPort());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+        proxy.newHar("Test");
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(CapabilityType.PROXY, seleniumProxy);
+        return caps;
+    }
+
+
     //Get Chrome Options
-    public static ChromeOptions getChromeOptions() {
-        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+    private ChromeOptions getChromeOptions(String perf) {
+        System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--disable-popup-blocking");
         //options.addArguments("--incognito");
+        if (perf.equalsIgnoreCase("YES")){
+            options.merge(performance());
+        }
         return options;
         /*ChromeDriverService service = new ChromeDriverService.Builder()
                 .usingAnyFreePort()
@@ -44,7 +94,7 @@ public class WebDriverController {
     }
 
     //Get Firefox Options
-    public static FirefoxOptions getFirefoxOptions() {
+    private FirefoxOptions getFirefoxOptions() {
         FirefoxOptions options = new FirefoxOptions();
         FirefoxProfile profile = new FirefoxProfile();
         //Accept Untrusted Certificates
@@ -57,35 +107,17 @@ public class WebDriverController {
         return options;
     }
 
-    public synchronized static void initDriver (String browser,String grid) {
-        try {
-            if (browser.equals("firefox")) {
-                _driverThread = new FirefoxDriver(getFirefoxOptions());
-                if (grid.equalsIgnoreCase("YES")){
-                    _driverThread = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),getFirefoxOptions());
-                }
-            } else if (browser.equals("chrome")) {
-                _driverThread = new ChromeDriver(getChromeOptions());
-                if (grid.equalsIgnoreCase("YES")){
-                    _driverThread = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),getChromeOptions());
-                }
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
 
     @AfterClass
     public void tearDown() {
-//        try {
-//            Har har = server.getHar();
-//            FileOutputStream fos = new FileOutputStream("C:\\temp\\perf.har");
-//            har.writeTo(fos);
-//            server.stop();
-//        } catch (Exception e) {
-//            logger.info("Performance test not included");
-//        }
+        try {
+            Har har = proxy.getHar();
+            FileOutputStream fos = new FileOutputStream("C:\\temp\\perf.har");
+            har.writeTo(fos);
+            proxy.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         _driverThread.quit();
     }
 
