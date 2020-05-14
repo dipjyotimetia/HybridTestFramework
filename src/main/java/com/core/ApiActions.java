6b6cac6e02ab8a55fa2625fb9f85adee86f107f4
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-05-14 16:43:26 +1000

    added api support

---
+++ src/main/java/com/core/ApiActions.java
@@ -0,0 +1,201 @@
+/*
+MIT License
+
+Copyright (c) 2020 Dipjyoti Metia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.core;
+
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ApiActions<T> {
+    private final Logger logger = LogManager.getLogger(ApiActions.class);
+
+    /**
+     * http request with parameter
+     *
+     * @param params jsonObject
+     * @return returns httpRequest
+     */
+    private RequestSpecification httpRequest(JSONObject params) {
+        return RestAssured
+                .given()
+                .with()
+                .contentType(ContentType.JSON)
+                .with()
+                .body(params.toJSONString());
+    }
+
+    /**
+     * Set base uri
+     *
+     * @param baseURI baseUri
+     */
+    public void setBaseURI(String baseURI) {
+        RestAssured.baseURI = baseURI;
+    }
+
+    /**
+     * Set base path
+     *
+     * @param basePathTerm basepath
+     */
+    public void setBasePath(String basePathTerm) {
+        RestAssured.basePath = basePathTerm;
+    }
+
+    /**
+     * reset base uri
+     */
+    public void resetBaseURI() {
+        RestAssured.baseURI = null;
+    }
+
+    /**
+     * Reset base path
+     */
+    public void resetBasePath() {
+        RestAssured.basePath = null;
+    }
+
+    /**
+     * http post
+     *
+     * @param params params
+     * @param path   endpoint
+     * @return response
+     */
+    protected Response httpPost(JSONObject params, String path) {
+        return httpRequest(params).request(Method.POST, path);
+    }
+
+    /**
+     * http get
+     *
+     * @param path endpoint
+     * @return response
+     */
+    protected Response httpGet(JSONObject params, String path) {
+        return httpRequest(params).request(Method.GET, path);
+    }
+
+    /**
+     * http delete
+     *
+     * @param params params
+     * @param path   endpoint
+     * @return response
+     */
+    protected Response httpDelete(JSONObject params, String path) {
+        return httpRequest(params).request(Method.DELETE, path);
+    }
+
+    /**
+     * http put
+     *
+     * @param params params
+     * @param path   endpoint
+     * @return response
+     */
+    protected Response httpPut(JSONObject params, String path) {
+        return httpRequest(params).request(Method.PUT, path);
+    }
+
+    /**
+     * Get Status code
+     *
+     * @param response response
+     * @return status code
+     */
+    protected int getStatusCode(Response response) {
+        return response.getStatusCode();
+    }
+
+    /**
+     * Get headers
+     *
+     * @param response response
+     * @param header   header
+     * @return header value
+     */
+    private String getHeaders(Response response, String header) {
+        return response.getHeaders().getValue(header);
+    }
+
+    /**
+     * Response Body
+     *
+     * @param response response
+     * @return responseBody
+     */
+    private ResponseBody responseBody(Response response) {
+        return response.getBody();
+    }
+
+    /**
+     * Get Body
+     *
+     * @param response response
+     * @return preety Print
+     */
+    protected String getBody(Response response) {
+        return responseBody(response).prettyPrint();
+    }
+
+    /**
+     * JsonPath evaluator
+     *
+     * @param response response
+     * @return jsonPath
+     */
+    protected T jsonPathEvaluator(Response response, String exp) {
+        return response.jsonPath().get(exp);
+    }
+
+    protected String sysDateFormat() {
+        try {
+            DateFormat date = new SimpleDateFormat("dd-MM-yyyy");
+            Date date1 = new Date();
+            String abc1 = date.format(date1);
+            return abc1;
+        } catch (Exception e) {
+            logger.error(e);
+        }
+        return null;
+    }
+
+    @Step("{0}")
+    protected void log(String message) {
+        logger.info(message);
+    }
+}
