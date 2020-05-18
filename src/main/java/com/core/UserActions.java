2d66adc2db987cbb807e77a7c8fa93eabe59cfe5
Dipjyoti Metia <dipjyotimetia@gmail.com>
2020-05-18 16:48:40 +1000

    Adding percy visual testing support

--- src/main/java/com/core/UserActions.java
+++ src/main/java/com/core/UserActions.java
@@ -26,6 +26,7 @@ package com.core;
 import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
 import com.github.javafaker.Faker;
+import io.percy.selenium.Percy;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.LogManager;
@@ -38,7 +39,6 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.awaitility.Awaitility;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 
@@ -59,18 +59,40 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 public class UserActions<T> extends DriverManager<T> {
-    private final Logger logger = LogManager.getLogger(UserActions.class);
-
     private static final Faker faker = new Faker();
     private static String datetimeabc = null;
     private static int Counter = 0;
     private static String abc1 = null;
-    private Dictionary dicttoread = new Hashtable();
     private static String _dbusername = "";
     private static String _dbpassword = "";
     private static String _dburl = "";
     private static WebDriverWait wait;
     private static JavascriptExecutor jsExec;
+    private final Logger logger = LogManager.getLogger(UserActions.class);
+    private final Percy percy = new Percy(driverThread);
+    private Dictionary dicttoread = new Hashtable();
+
+    public static Map<String, String> get(Map<String, String> formParams) {
+        return formParams
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() != null)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    /**
+     * sleep
+     *
+     * @param seconds time
+     */
+    private static void sleep(Integer seconds) {
+        long secondsLong = (long) seconds;
+        try {
+            Thread.sleep(secondsLong);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     protected void navigate(String url) {
         SystemDateFormat();
@@ -230,7 +252,7 @@ public class UserActions<T> extends DriverManager<T> {
      * @param elements elements
      */
     public void getAllValues(List<WebElement> elements) {
-        elements.forEach(e -> System.out.println(e));
+        elements.forEach(e -> logger.info(e));
     }
 
     /**
@@ -251,14 +273,6 @@ public class UserActions<T> extends DriverManager<T> {
      */
     public List<String> getAllLinks(List<WebElement> elements) {
         return elements.stream().map(ele -> ele.getText().trim()).collect(Collectors.toList());
-    }
-
-    public static Map<String, String> get(Map<String, String> formParams) {
-        return formParams
-                .entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() != null)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     /**
@@ -374,20 +388,6 @@ public class UserActions<T> extends DriverManager<T> {
     public void waitJQueryAngular() {
         waitUntilJQueryReady();
         waitUntilAngularReady();
-    }
-
-    /**
-     * sleep
-     *
-     * @param seconds time
-     */
-    private static void sleep(Integer seconds) {
-        long secondsLong = (long) seconds;
-        try {
-            Thread.sleep(secondsLong);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     /**
@@ -871,10 +871,6 @@ public class UserActions<T> extends DriverManager<T> {
         return false;
     }
 
-    public enum WebElementBy {
-        XPATH, ID, NAME, CLASS, CSS
-    }
-
     /**
      * isElement displayed
      *
@@ -898,10 +894,6 @@ public class UserActions<T> extends DriverManager<T> {
                 logger.info("Element display type not available");
         }
         return returnValue;
-    }
-
-    public enum DisplayType {
-        IS_ENABLED, IS_SELECTED, IS_DISPLAYED
     }
 
     /**
@@ -945,9 +937,44 @@ public class UserActions<T> extends DriverManager<T> {
         }
     }
 
+    /**
+     * Capture screen
+     * @param name screenName
+     */
+    protected void captureScreen(String name) {
+        percy.snapshot(name);
+    }
+
+    /**
+     * Capture screen
+     * @param name screenName
+     * @param width width
+     */
+    protected void captureScreen(String name, List<Integer> width) {
+        percy.snapshot(name, width);
+    }
+
+    /**
+     * Capture Screen
+     * @param name screen name
+     * @param width width
+     * @param minHeight minimum height
+     */
+    protected void captureScreen(String name, List<Integer> width, int minHeight) {
+        percy.snapshot(name, width, minHeight);
+    }
+
     protected void catchBlock(Exception e) {
         Counter = 0;
         logger.error("Error Description", e);
         Assert.fail("TestCase Failed", e);
+    }
+
+    public enum WebElementBy {
+        XPATH, ID, NAME, CLASS, CSS
+    }
+
+    public enum DisplayType {
+        IS_ENABLED, IS_SELECTED, IS_DISPLAYED
     }
 }
