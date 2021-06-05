33bdc1a34ac0dd51483369ed338b75c2c0c8419d
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-06-05 22:19:58 +1000

    Adding ssh login

---
+++ src/main/java/com/utils/SSH.java
@@ -0,0 +1,86 @@
+/*
+MIT License
+
+Copyright (c) 2020 Dipjyoti Metia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.channel.ClientChannelEvent;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.common.channel.Channel;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SSH {
+
+    /**
+     * List Folder Structure
+     *
+     * @param username              username
+     * @param password              password
+     * @param host                  host
+     * @param port                  port
+     * @param defaultTimeoutSeconds defaultTime
+     * @param command               command
+     * @throws IOException exception
+     */
+    public void listFolderStructure(String username, String password,
+                                    String host, int port, long defaultTimeoutSeconds, String command) throws IOException {
+
+        SshClient client = SshClient.setUpDefaultClient();
+        client.start();
+
+        try (ClientSession session = client.connect(username, host, port)
+                .verify(defaultTimeoutSeconds, TimeUnit.SECONDS).getSession()) {
+            session.addPasswordIdentity(password);
+            session.auth().verify(defaultTimeoutSeconds, TimeUnit.SECONDS);
+
+            try (ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+                 ClientChannel channel = session.createChannel(Channel.CHANNEL_SHELL)) {
+                channel.setOut(responseStream);
+                try {
+                    channel.open().verify(defaultTimeoutSeconds, TimeUnit.SECONDS);
+                    try (OutputStream pipedIn = channel.getInvertedIn()) {
+                        pipedIn.write(command.getBytes());
+                        pipedIn.flush();
+                    }
+
+                    channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
+                            TimeUnit.SECONDS.toMillis(defaultTimeoutSeconds));
+                    String responseString = responseStream.toString();
+                    log.info(responseString);
+                } finally {
+                    channel.close(false);
+                }
+            }
+        } finally {
+            client.stop();
+        }
+    }
+}
