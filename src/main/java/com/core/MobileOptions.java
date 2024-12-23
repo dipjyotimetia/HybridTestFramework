2118dc13b1d1bcac032cc0b5e8182334b9d0949f
Dipjyoti Metia <dipjyotimetia@gmail.com>
2024-12-23 21:10:57 +1100

    fix test executions

--- src/main/java/com/core/MobileOptions.java
+++ src/main/java/com/core/MobileOptions.java
@@ -119,14 +119,30 @@ abstract class MobileOptions {
      */
     private void browserStackMobileCapabilities(DesiredCapabilities caps, String device) {
         genericMobileCapabilities(caps, device);
-        HashMap<String, Object> browserstackOptions = new HashMap<>();
-        browserstackOptions.put("automationVersion", "latest");
-        browserstackOptions.put("appiumVersion", "2.3.0");
-        browserstackOptions.put("projectName", "HybridTestFramework");
-        //browserstackOptions.put("local", "true");
-        browserstackOptions.put("buildName", "browserstack-build-1");
-        browserstackOptions.put("sessionName", "first_test");
-        caps.setCapability("bstack:options", browserstackOptions);
+        switch (device) {
+            case "s23" -> {
+                caps.setCapability("appium:platformVersion", "13");
+                caps.setCapability("appium:deviceName", "Samsung Galaxy S23");
+            }
+            case "pixel" -> {
+                caps.setCapability("appium:platformVersion", "15");
+                caps.setCapability("appium:deviceName", "Google Pixel 9 Pro");
+            }
+            case "iPhone16" -> {
+                caps.setCapability("appium:platformVersion", "18");
+                caps.setCapability("appium:deviceName", "iPhone 16");
+            }
+            default -> System.out.println("No device found");
+        }
+        HashMap<String, Object> bstackOptions = new HashMap<>();
+        bstackOptions.put("userName", bs_username);
+        bstackOptions.put("accessKey", bs_accessKey);
+        bstackOptions.put("appiumVersion", "2.6.0");
+        bstackOptions.put("projectName", "HybridTestFramework");
+        //bsOptions.put("local", "true");
+        bstackOptions.put("buildName", "browserstack-build-1");
+        bstackOptions.put("sessionName", "first_test");
+        caps.setCapability("bstack:options", bstackOptions);
         log.info("Setting up browserstack capabilities");
     }
 
@@ -140,7 +156,7 @@ abstract class MobileOptions {
         genericMobileCapabilities(caps, device);
         HashMap<String, Object> ltOptions = new HashMap<>();
         switch (device) {
-            case "samsung" -> {
+            case "s23" -> {
                 ltOptions.put("platformName", "android");
                 ltOptions.put("deviceName", "Galaxy S23");
                 ltOptions.put("platformVersion", "14");
@@ -158,8 +174,10 @@ abstract class MobileOptions {
             default -> System.out.println("No device found");
         }
         // ltOptions.put("isRealMobile", false);
+        ltOptions.put("user", lambda_username);
+        ltOptions.put("accessKey", lambda_accessKey);
         ltOptions.put("w3c", true);
-        ltOptions.put("appiumVersion", "2.3.0");
+        ltOptions.put("appiumVersion", "2.6.0");
         ltOptions.put("build", "HybridTestFramework");
         ltOptions.put("deviceOrientation", "portrait");
         ltOptions.put("autoGrantPermissions", true);
@@ -190,12 +208,14 @@ abstract class MobileOptions {
      */
     private void genericMobileCapabilities(DesiredCapabilities caps, String device) {
         switch (device) {
-            case "samsung", "pixel" -> {
+            case "s23", "pixel" -> {
+                caps.setCapability("platformName", "android");
                 caps.setCapability("browserName", "chrome");
                 caps.setCapability("appium:automationName", "uiautomator2");
                 caps.setCapability("appium:app", apk_url);
             }
             case "iPhone16" -> {
+                caps.setCapability("platformName", "ios");
                 caps.setCapability("browserName", "safari");
                 caps.setCapability("appium:automationName", "xcuitest");
                 caps.setCapability("appium:app", ipa_url);
